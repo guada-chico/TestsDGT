55acdfc1a5045006834db3c56cb7e06176eed9b3
Guada <guadalupe.fernandez@des.kyocera.com>
2026-05-11 12:59:13 +0200

    Error filtros catalogo completo

--- Pruebas/Catalogo/ErrorFiltrosCatalogo.cs
+++ Pruebas/Catalogo/ErrorFiltrosCatalogo.cs
@@ -8,7 +8,22 @@ public class ErrorFiltrosCatalogoTest : BaseTest
 {
     [Test]
 
-    public async Task ErrorFiltrosCatalogo_SinResultados()
+    public async Task ErrorFiltrosCatalogoCodigo_SinResultados()
+    {
+        string busquedaErronea = "CodigoInexistente";
+
+        await Page.GetByPlaceholder("Buscar por código").FillAsync(busquedaErronea);
+        await Page.Locator("button").Filter(new() { Has = Page.Locator(".pi-filter") }).ClickAsync();
+
+        var mensajeVacio = Page.Locator("td").Filter(new() { HasText = "No hay resultados para tu búsqueda" });
+        await Assertions.Expect(mensajeVacio).ToBeVisibleAsync();
+
+        await Page.Locator("button.btn-clear").ClickAsync();
+    }
+
+    [Test]
+
+    public async Task ErrorFiltrosCatalogoNombre_SinResultados()
     {
         string busquedaErronea = "ProductoInexistente";
 
