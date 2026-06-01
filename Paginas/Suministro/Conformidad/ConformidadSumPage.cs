7cb44b0063421d5611aedae2720eaaff42914ddc
Guada <guadalupe.fernandez@des.kyocera.com>
2026-06-01 11:55:39 +0200

    cambios suministroPage

--- Paginas/Suministro/Conformidad/ConformidadSumPage.cs
+++ Paginas/Suministro/Conformidad/ConformidadSumPage.cs
@@ -5,14 +5,10 @@ using System.Threading.Tasks;
 
 namespace TestsDGT.Paginas.Suministro.Conformidad;
 
-public class ConformidadSumPage
+public class ConformidadSumPage : SuministroBasePage
 {
-    private readonly IPage _page;
-
     private ILocator InputFiltroCodigoOrden => _page.GetByPlaceholder("Buscar por Código de Orden");
     private ILocator FiltroComboProveedor => _page.Locator("p-dropdown[placeholder='Todos los proveedores']");
-    private ILocator BotonFiltrar => _page.Locator("button").Filter(new() { Has = _page.Locator(".pi-filter") });
-    private ILocator BotonLimpiarFiltro => _page.Locator("button").Filter(new() { Has = _page.Locator(".pi-times") });
 
     private ILocator FechaVerificacion => _page.Locator("#fechaVerificacion-0 input[placeholder='dd/mm/aaaa']");
     private ILocator InputObservaciones => _page.GetByPlaceholder("Escriba aquí las observaciones sobre esta verficación...");
@@ -21,19 +17,13 @@ public class ConformidadSumPage
     private ILocator BotonRegistrarConformidad => _page.GetByRole(AriaRole.Button, new() { Name = "Registrar conformidad" });
     private ILocator BotonCancelarVerificacion => _page.GetByRole(AriaRole.Button, new() { Name = "Cancelar" });
 
-    private ILocator ToastMensaje => _page.Locator(".p-toast:visible");
-    private ILocator ToastMensajeDetalle => ToastMensaje.Locator(".p-toast-detail"); 
    
     private ILocator FilasTablaConformidadSum => _page.Locator("tbody tr");
 
-    private ILocator BotonMasOpciones => _page.Locator("button").Filter(new() { Has = _page.Locator(".pi-ellipsis-v") });
     private ILocator OpcionVerDetalleConformidadSum => _page.GetByRole(AriaRole.Menuitem, new() { Name = "Ver detalle" });
 
 
-    public ConformidadSumPage(IPage page)
-    {
-        _page = page;
-    }
+    public ConformidadSumPage(IPage page) : base(page) { }
 
     public async Task FiltrarPorCodigoOrdenAsync(string codigo)
     {
@@ -53,12 +43,6 @@ public class ConformidadSumPage
         await _page.WaitForTimeoutAsync(200);
 
         await BotonFiltrar.ClickAsync();
-        await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
-    }
-
-    public async Task LimpiarFiltrosAsync()
-    {
-        await BotonLimpiarFiltro.ClickAsync();
         await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
     }
 
@@ -109,37 +93,8 @@ public class ConformidadSumPage
         catch { return false; }
     }
 
-    public async Task<bool> VerificarTextoEnTablaAsync(string textoBuscar)
-    {
-        try
-        {
-            var celda = _page.Locator("td").GetByText(textoBuscar, new() { Exact = false }).First;
-            await Assertions.Expect(celda).ToBeVisibleAsync(new() { Timeout = 5000 });
-            return true;
-        }
-        catch (Exception)
-        {
-            return false;
-        }
-    }
-
-    public async Task<string> ObtenerMensajeToastAsync()
-    {
-        var ultimoToast = ToastMensajeDetalle.Last;
-
-        await ultimoToast.WaitForAsync(new() { State = WaitForSelectorState.Visible, Timeout = 15000 });
-
-        return await ultimoToast.InnerTextAsync();
-    }
-
     public async Task<int> ObtenerNumeroFilasConformidadSumAsync()
     {
         return await FilasTablaConformidadSum.CountAsync();
-    }
-
-    public async Task IrAConformidadSuministro()
-    {
-        await _page.GotoAsync("http://192.168.200.51:7001/dgt-front/#/conformidad-suministro");
-        await _page.WaitForURLAsync("**/conformidad-suministro");
     }
 }
---
+++ Paginas/Suministro/SuministroPage.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace TestsDGT.Paginas.Suministro;
+
+public abstract class SuministroBasePage 
+{
+    protected readonly IPage _page; 
+
+    protected ILocator BotonFiltrar => _page.Locator("button").Filter(new() { Has = _page.Locator(".pi-filter") });
+    protected ILocator BotonLimpiarFiltro => _page.Locator("button").Filter(new() { Has = _page.Locator(".pi-times") });
+    protected ILocator ToastMensaje => _page.Locator(".p-toast:visible");
+    protected ILocator ToastMensajeDetalle => ToastMensaje.Locator(".p-toast-detail");
+    protected ILocator FilasTabla => _page.Locator("tbody tr");
+    protected ILocator BotonMasOpciones => _page.Locator("button").Filter(new() { Has = _page.Locator(".pi-ellipsis-v") });
+    protected ILocator OpcionVerDetalle => _page.GetByRole(AriaRole.Menuitem, new() { Name = "Ver detalle" });
+
+    protected SuministroBasePage(IPage page)
+    {
+        _page = page;
+    }
+
+    public async Task LimpiarFiltrosAsync()
+    {
+        await BotonLimpiarFiltro.ClickAsync();
+        await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+    }
+
+    public async Task<bool> VerificarTextoEnTablaAsync(string textoBuscar)
+    {
+        try
+        {
+            var celda = _page.Locator("td").GetByText(textoBuscar, new() { Exact = false }).First;
+            await Assertions.Expect(celda).ToBeVisibleAsync(new() { Timeout = 5000 });
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public async Task<string> ObtenerMensajeToastAsync()
+    {
+        var ultimoToast = ToastMensajeDetalle.Last;
+        await ultimoToast.WaitForAsync(new() { State = WaitForSelectorState.Visible, Timeout = 15000 });
+        return await ultimoToast.InnerTextAsync();
+    }
+
+    public async Task<int> ObtenerNumeroFilasTablaAsync()
+    {
+        return await FilasTabla.CountAsync();
+    }
+
+    public async Task IrAConformidadSuministro()
+    {
+        await _page.GotoAsync("http://192.168.200.51:7001/dgt-front/#/conformidad-suministro");
+        await _page.WaitForURLAsync("**/conformidad-suministro");
+    }
+}
